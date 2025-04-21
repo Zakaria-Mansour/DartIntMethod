4860172f2ac7b543c303b35324f3e8e5f73780fb
Zakaria-Mansour <zakaria.mansour97@yahoo.com>
2025-04-21 21:38:59 +0300

    update

--- main.dart
+++ main.dart
@@ -11,4 +11,8 @@ print(myHighest.ceil());
 print(myHighest.floor());
 print(myHighest.isNegative);
 print(myHighest.abs());
+
+
+int myAgeafter10year = myAge + 10;
+print("my age after 10 years is $myAgeafter10year");
 }
