4e2547b3283cd620cb040a2d25541696733c89c8
Zakaria-Mansour <zakaria.mansour97@yahoo.com>
2025-04-21 21:30:24 +0300

    update

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){
+
+String myname= "zakaria mansour";
+int myAge = 27;
+double myHighest = -171.4;
+
+print(myAge);
+print(myHighest.round());
+print(myname[0].toUpperCase());
+print(myHighest.ceil());
+print(myHighest.floor());
+print(myHighest.isNegative);
+print(myHighest.abs());
+}
